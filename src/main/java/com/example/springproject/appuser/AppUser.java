4118806dec2ca4f518b73954a3f4b4fb8261f0dd
DESKTOP-94KJSSS\leont <leonte_petru@yahoo.com>
2021-11-10 18:15:56 +0200

    test

--- src/main/java/com/example/springproject/appuser/AppUser.java
+++ src/main/java/com/example/springproject/appuser/AppUser.java
@@ -19,7 +19,7 @@ import java.util.Collections;
 @Entity
 public class AppUser implements UserDetails {
 
-
+//// test push
     @SequenceGenerator(
             name = "student_sequence",
             sequenceName = "student_sequence",
